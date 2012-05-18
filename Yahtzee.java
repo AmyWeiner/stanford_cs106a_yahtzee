bdd949131dfbe86befad4e00eb5f389a8afe3b6a
S <s>
2012-05-18 09:38:50 -0700

    {"type":"TI", "runs":"1"}

--- Yahtzee.java
+++ Yahtzee.java
@@ -80,7 +80,7 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 		ui.updateScorecard(category, player, score);
 		int total = calculateTotal(player);
 		ui.updateScorecard(TOTAL, player, total);
-		scoreCard[TOTAL][player] = total;
+		//scoreCard[TOTAL][player] = total;
 	}
 
 	private void reRoll(int player, String name, int[] dice) {
